7e8deedd68b19e4ef5477183943da95ed1652c79
CyberDuck79 <flavien.henrion@protonmail.com>
2019-08-22 20:27:19 +0200

    corrections

--- CAcrypto.c
+++ CAcrypto.c
@@ -1,19 +1,32 @@
-#include <fctln.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CAcrypto.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/22 20:21:07 by fhenrion          #+#    #+#             */
+/*   Updated: 2019/08/22 20:21:07 by fhenrion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <limits.h>
 
 typedef unsigned long long ull;
-#define N  (sizeof(ull) * CHAR_BIT)
-#define U  sizeof(ull)
+#define N (sizeof(ull) * CHAR_BIT)
+#define U sizeof(ull)
 #define B(x) (1ULL << (x))
 
-ull encrypt_block(ull block, int *key, int key_len, char gen_seed)
+ull encrypt_block(ull block, char *key, int key_len, char gen_seed)
 {
     ull             st;
-	static ull		key_i = 0;
+	static int		key_i = 0;
     static ull      state = 2147483648;
-    int             rule_i;
+    int             rule_i = 0;
     char            rules[8] = {30, 135, 86, 149, 45, 101, 75, 89};
 
 	rule_i += key[key_i]; // get rule index by adding permutation value at key index
@@ -21,7 +34,7 @@ ull encrypt_block(ull block, int *key, int key_len, char gen_seed)
         rule_i -= 8;
     st = state;
     state = 0;
-    for (int i = 0; i < N; i++)
+    for (unsigned long i = 0; i < N; i++)
         if (rules[rule_i] & B(7 & (st>>(i-1) | st<<(N+1-i))))
             state |= B(i);
     if (!gen_seed) // if seed gen, do not apply XOR mask
@@ -31,7 +44,7 @@ ull encrypt_block(ull block, int *key, int key_len, char gen_seed)
     return (block);
 }
 
-int     encryption(int fd[2], int *key, int key_len)
+int     encryption(int fd[2], char *key, int key_len)
 {
     ull block;
 
@@ -58,14 +71,15 @@ int     main(int argc, char **argv)
 
     if (argc == 3)
     {
-        if ((fd[0] = open(argv[1], O_RDONLY)) == -1 || (fd[1] = open("output.ca",  O_WRONLY | O_CREAT, 0644)) == -1)
+        if ((fd[0] = open(argv[1], O_RDONLY)) == -1 || \
+        (fd[1] = open("output.ca",  O_WRONLY | O_CREAT, 0644)) == -1)
         {
 			perror("open failed");
 			exit(1);
         }
         while (argv[2][key_len])
             key_len++;
-        if (!(key = (int*)malloc(sizeof(int) * key_len)))
+        if (!(key = (char*)malloc(sizeof(char) * key_len)))
         {
             perror("memory allocation failed");
 			exit(1);
