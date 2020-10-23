7f598e0a860ac112f51e12c0836e2ee2d26296c0
Flavien Henrion <flavien.henrion@protonmail.com>
2020-10-23 10:56:42 +0200

    hide cursor

--- progress_bar.c
+++ progress_bar.c
@@ -6,7 +6,7 @@
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 15:13:50 by fhenrion          #+#    #+#             */
-/*   Updated: 2020/02/13 18:47:05 by fhenrion         ###   ########.fr       */
+/*   Updated: 2020/10/23 10:52:11 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 void	ft_progress(size_t total)
 {
 	static size_t	count = 1;
-	int				bar = count * BAR_LENGTH / total;;
+	int				bar = count * BAR_LENGTH / total;
 	int				load = BAR_LENGTH - bar;
 	int				percent = count * 100 / total;
 
+	if (count == 1)
+		printf("\e[?25l");
 	printf("\rProgress [%.*s%.*s] %i%%", bar, BAR, load, LOAD, percent);
 	fflush(stdout);
 	if (count < total)
